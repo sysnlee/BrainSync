1b3dc601957dd40fa533ec230dfd8b20a4557e97
Sysn Lee <35058326+sysnlee@users.noreply.github.com>
2018-08-06 16:35:44 +0800

    Update HealthController.cs

--- BrainSync/BrainSyncSite/Controllers/HealthController.cs
+++ BrainSync/BrainSyncSite/Controllers/HealthController.cs
@@ -9,6 +9,7 @@ namespace BrainSyncSite.Controllers
     public class HealthController : Controller
     {
         // GET: Health
+        //在这里实现一个基于HTML5的3D浏览工具
         public ActionResult Index()
         {
             ViewBag.Message = "健康是一切開始的前提！";
